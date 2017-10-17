bc82e651b680791474c2b6eb79719c83622690b4
xjl245 <qq1446327316@163.com>
2017-10-17 22:46:58 +0800

    make collect program better

--- netReptile/public/javascripts/collect2.js
+++ netReptile/public/javascripts/collect2.js
@@ -10,15 +10,15 @@ var fs = require('fs');
 
 //var i = 0;
 var arr = ['那年花开月正圆', '加勒比海盗', '变形金刚', '三生三世十里桃花', '老友记', '侠盗联盟', '高尔夫球手', '极寒之城', '黑暗塔'];
-var data = [];
-var length = 5; //每次取出的长度
+//var data = [];
+var length = 1; //每次取出的长度
 var index = 0; //当前取出的id
 
 var start = 0;
 var bag = new bagpipe(2);
 var cookies;
 
-function GetData(){
+function GetData(res){
 	console.log('start-----------------------------------------------');
 	var sql = 'select name from filmname limit ' + start + ',' + length;
 	db.con(sql, function(err, rows){
@@ -27,12 +27,12 @@ function GetData(){
 			return;
 		}
 		if(!err){
-			console.log(rows);
-			data = rows;
-			emitter.emit('Collect');
+			//console.log(rows);
+			res(null, rows);
 		}	
 		else if(err){
-			console.log(err);
+			console.log('end----------------------');
+			res(err);
 		}
 		start += length;
 	});
@@ -76,7 +76,7 @@ function GetData(){
 
 function StartCollect()
 {
-	//console.log('collect');
+/* 	//console.log('collect');
 	var i = 0;
 	var timer = setInterval(function(){
         console.log(data[i++].name);//处理url采集
@@ -85,42 +85,51 @@ function StartCollect()
 			data = [];
 			emitter.emit('Read');
 		}		
-    }, 3000);
+    }, 3000); */
 }
 
-function CollectDetailUrls(res){
+function CollectDetailUrls(data, res){
 	//1.取出第一层所有的url
 	//2.循环第一层所有url取出对应种子信息
 	console.log(data);
 	var allUrls = [];
-	var time = (Math.floor(Math.random()*5+5) + 1) * 1000;
+	var time = (Math.floor(Math.random()*2 + 1) + 1) * 1000;
 	console.log(time);
 	var i = 0;
+	var state = 'free';
+	
 	var timer = setInterval(function(){
-		var nameEncode = encodeURI(data[i].name);
+		console.log('CollectDetailUrls:' + i);
+		var nameEncode = encodeURI(data[i].name);	
 		var jsonBag = {url:'https://gaoqing.fm/s.php?q=' + nameEncode, encoding: null, headers: {
 		  'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
 		  'Accept':'*/*',
 		  'Referer':'https://www.baidu.com'
-		}};
+		}};	
 		
-		request(jsonBag, GetDetailUrls(function(err, urls){
+		if(state === 'free'){
 			i++;
-			for(var k = 0; k < urls.length; k++){
-				//console.log(urls[k]);
-				allUrls.push(urls[k]);
-			}
-			if(i === data.length){
-				//console.log(data.length);
-				clearInterval(timer);
-				data = [];				
-				res(null, allUrls);
-				emitter.emit('Read');
-			}	
-		}));
+			state = 'padding';
+			request(jsonBag, GetDetailUrls(function(err, urls){
+				if(err){
+					console.log('Get Urls: ' + err);
+					return;
+				}
+				
+				for(var k = 0; k < urls.length; k++){
+					//console.log(urls[k]);
+					allUrls.push(urls[k]);
+				}
+				if(i === data.length){
+					//console.log(data.length);
+					clearInterval(timer);
+					//data = [];				
+					res(null, allUrls);				
+				}
+				state = 'free';
+			}));
+		}
 		
-		
-		//console.log('dddddddddddddddddddddddddd');
 	}, time);
 }
 
@@ -168,22 +177,116 @@ function AnalyzeHtml(ch, allDetailUrls, id){
 					});
 }
 
-function func(){
-	//console.log('start');
-	emitter.on('Collect', function(){
-		//console.log('Collect');
-		//StartCollect();
-		CollectDetailUrls(function(err, urls){
-			for(var i = 0; i < urls.length; i++){				
-				console.log(urls[i]);
+function GetInfos(urls, res){
+	console.log(urls);
+	var allInfos = [];
+	var time = (Math.floor(Math.random()* 5 + 2) + 1) * 1000;
+	var i = 0;
+	//var j = 0;
+	var state = 'free';
+	var timer = setInterval(function(){
+		console.log('GetInfos: ' + i);
+		var jsonBag = {url : urls[i].url, encoding: null, headers: {
+		  'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+		  'Accept':'*/*'
+		}};
+		
+		if(state === 'free'){			
+			state = 'padding';
+			request(jsonBag, GetInfo(function(err, gut, infos){	
+				
+				if(err){
+					console.log('Get Deatils: ' + err);
+					return;
+				}
+				
+				//此处当前面一个url还未采集完，定时时间已经到下面一个，i的值已经等于urls.length 会少采集
+				allInfos.push({realName:urls[i].realName, gut:gut, infos:infos, type: urls[i].type});
+				i++;
+				if(i === urls.length){
+					clearInterval(timer);			
+					res(null, allInfos);
+					//emitter.emit('Read');
+				}
+
+				state = 'free';				
+			}));	
+		}		
+	}, time);
+}
+
+function GetInfo(res){
+	return function(error, response, body){
+			if (!error) {				
+			var html = iconv.decode(body, 'utf8');
+			var ch = cheerio.load(html);
+
+			//剧情
+			var gut = '';
+			if((gut = ch('#des-ex').text()) === ''){
+				gut = ch('#des-full').text();
 			}
+			var infos = [];
+			ch('#cili').find('tr').each(function(i, elem){
+				var info = {};					
+				if(!ch(elem).hasClass('hidden-cililian-btn') && !ch(elem).hasClass('show-cililian-btn')){
+					info.urlName = ch(elem).find('b').text();
+
+					ch(elem).find('a').each(function(j, val){
+						if(j === 0){
+							info.zz = ch(val).attr('href');
+						}
+						else {
+							info.cili = ch(val).attr('href')
+						}
+					});
+
+					ch(elem).find('span').each(function(j, value){
+						var spanInfo = ch(value);
+						if(spanInfo.hasClass('label') && spanInfo.hasClass('label-warning'))	{
+							info.size = spanInfo.text();
+						}
+						if(spanInfo.hasClass('label') && spanInfo.hasClass('label-danger')){
+							info.pixel = spanInfo.text();
+						}
+					});
+				}
+				infos.push(info);
+			});
+			res(null, gut, infos)
+		}
+		else{
+			res(error);
+		}
+	};
+}
+
+function func(){
+	emitter.on('Collect', function(data){
+		CollectDetailUrls(data, function(err, urls){			
+			emitter.emit('details', urls);
 		});
 	});
+	
 	emitter.on('Read', function() {
-		//console.log('Read');
-		GetData();
+		GetData(function(err, data){		
+			emitter.emit('Collect', data);
+		});
 	});
-	GetData();
+	
+	emitter.on('details', function(urls){
+		//console.log('details:  '+ urls[2].realName);
+		GetInfos(urls, function(err, allInfos){
+			for(var i = 0; i < allInfos.length; i++){
+				console.log(allInfos[i].realName);
+			}
+			emitter.emit('Read');
+		});
+	});
+	
+	GetData(function(err, data){	
+		emitter.emit('Collect', data);
+	});
 }
 
 function WriteInfo(html)
